2b539b7982d0544c1a6f23b3ef33b79e3012d0c5
Smit410 <smit.shah.chintan@gmail.com>
2025-12-28 15:10:06 +1300

    first commit

---
+++ activity1.py
@@ -0,0 +1,8 @@
+name= "timmy"
+age= 834187283748912798471974123480981234298434710283740128374012983470192834710293874012389
+height= 831823470192384.97
+is_employed= False
+print("the data type of:", name,"is", type(name))
+print("the data type of:", age,"is", type(age))
+print("the data type of:", height,"is", type(height))
+print("the data type of:", is_employed,"is", type(is_employed))
